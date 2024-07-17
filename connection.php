bd16166c3a7a62331cc5fc5d5fd801c03fe425bb
tamimurashid <121192802+tamimurashid@users.noreply.github.com>
2024-07-17 11:34:43 +0300

    new changes

--- connection.php
+++ connection.php
@@ -1,53 +1,11 @@
-<?php
-class Connection {
-    private $dbhost = "localhost";
-    private $dbuser = "root";
-    private $dbpassword = "root";
-    private $dbname = "form";
-    protected $conn;
+<?php 
+ $dbhost = "localhost";
+ $dbuser = "root";
+ $dbpassword = "root";
+ $dbname = "form";
 
-    // Constructor to automatically connect to the database
-    public function __construct() {
-        $this->connect();
-    }
+$conn = mysqli_connect($dbhost, $dbuser, $dbpassword, $dbname);
 
-    // Method to establish a database connection
-    private function connect() {
-        $this->conn = new mysqli($this->dbhost, $this->dbuser, $this->dbpassword, $this->dbname);
-
-        // Check connection
-        if ($this->conn->connect_error) {
-            die("Connection failed: " . $this->conn->connect_error);
-        }
-    }
-
-    // Method to get the connection
-    public function getConnection() {
-        return $this->conn;
-    }
+if (mysqli_connect_errno() == 0) {
+    echo "connection successfully "; 
 }
-
-class Write extends Connection {
-    public function executeWrite($query) {
-        $result = $this->getConnection()->query($query);
-
-        if ($result === TRUE) {
-            return TRUE;
-        } else {
-            return FALSE;
-        }
-    }
-}
-
-class Retrieve extends Connection {
-    public function executeRetrieve($query) {
-        $result = $this->getConnection()->query($query);
-
-        if ($result->num_rows > 0) {
-            return $result;
-        } else {
-            return NULL;
-        }
-    }
-}
-
--- controller.php
+++ controller.php
@@ -1,20 +1,38 @@
 <?php 
 require "connection.php";
+// variable for validation 
+$userpattern = '/[^a-zA-Z\s]/';
+$phonepattern = "/^(07[15678]\d{7}|061\d{7}|062\d{7})$/";
+$message = "";
+$message_type = "";
 
-$connection = new Connection();
+
 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
-   $FNAME = mysqli_real_escape_string($connection->getConnection(), $_POST["fname"]);
-   $LNAME = mysqli_real_escape_string($connection->getConnection(), $_POST["lname"]);
-   $EMAIL = mysqli_real_escape_string($connection->getConnection(), $_POST["email"]);
-   $PASSWORD = mysqli_real_escape_string($connection->getConnection(), $_POST["password"]);
-   $CPASSWORD = mysqli_real_escape_string($connection->getConnection(), $_POST["cpassword"]);
-   $CITY = mysqli_real_escape_string($connection->getConnection(), $_POST["city"]);
-   $GENDER = mysqli_real_escape_string($connection->getConnection(), $_POST["gender"]);
-   $PHONE = mysqli_real_escape_string($connection->getConnection(), $_POST["phone"]);
-   $CHECK = mysqli_real_escape_string($connection->getConnection(), $_POST["check"]);
+   $FNAME = mysqli_real_escape_string($conn, $_POST["fname"]);
+   $LNAME = mysqli_real_escape_string($conn, $_POST["lname"]);
+   $EMAIL = mysqli_real_escape_string($conn, $_POST["email"]);
+   $PASSWORD = mysqli_real_escape_string($conn, $_POST["password"]);
+   $CPASSWORD = mysqli_real_escape_string($conn, $_POST["cpassword"]);
+   $CITY = mysqli_real_escape_string($conn, $_POST["city"]);
+   $GENDER = mysqli_real_escape_string($conn, $_POST["gender"]);
+   $PHONE = mysqli_real_escape_string($conn, $_POST["phone"]);
+   $CHECK = mysqli_real_escape_string($conn, $_POST["check"]);
 
    
+   // validation 
+   if( preg_match($userpattern,$FNAME) || strlen($FNAME) < 2  || strlen($FNAME) > 20){
+    $message = "invalid name input, first name should not be less than 3 character or greater than 20 character, only letters required ";
+    $message_type = "error";
+   }
+   elseif( preg_match($userpattern,$LNAME) || strlen($LNAME) < 2  || strlen($LNAME) > 20){
+    $message = "invalid name input, first name should not be less than 3 character or greater than 20 character, only letters required ";
+    $message_type = "error";
+   }
+   elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    $message = "Invalid email address!";
+    $message_type = "error";
+}
    
   
   
