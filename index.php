9687f6396eb21abbc6a5bf4d107af67075424e2a
tamimurashid <121192802+tamimurashid@users.noreply.github.com>
2024-07-17 11:50:13 +0300

    in this update i delete controller.php

--- index.php
+++ index.php
@@ -1,3 +1,46 @@
+<?php 
+require "connection.php";
+// variable for validation 
+$userpattern = '/[^a-zA-Z\s]/';
+$phonepattern = "/^(07[15678]\d{7}|061\d{7}|062\d{7})$/";
+$message = "";
+$message_type = "";
+
+
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+   $FNAME = mysqli_real_escape_string($conn, $_POST["fname"]);
+   $LNAME = mysqli_real_escape_string($conn, $_POST["lname"]);
+   $EMAIL = mysqli_real_escape_string($conn, $_POST["email"]);
+   $PASSWORD = mysqli_real_escape_string($conn, $_POST["password"]);
+   $CPASSWORD = mysqli_real_escape_string($conn, $_POST["cpassword"]);
+   $CITY = mysqli_real_escape_string($conn, $_POST["city"]);
+   $GENDER = mysqli_real_escape_string($conn, $_POST["gender"]);
+   $PHONE = mysqli_real_escape_string($conn, $_POST["phone"]);
+   $CHECK = mysqli_real_escape_string($conn, $_POST["check"]);
+
+   
+   // validation 
+   if( preg_match($userpattern,$FNAME) || strlen($FNAME) < 2  || strlen($FNAME) > 20){
+    $message = "invalid name input, first name should not be less than 3 character or greater than 20 character, only letters required ";
+    $message_type = "error";
+   }
+   elseif( preg_match($userpattern,$LNAME) || strlen($LNAME) < 2  || strlen($LNAME) > 20){
+    $message = "invalid name input, first name should not be less than 3 character or greater than 20 character, only letters required ";
+    $message_type = "error";
+   }
+   elseif (!filter_var($EMAIL, FILTER_VALIDATE_EMAIL)) {
+    $message = "Invalid email address!";
+    $message_type = "error";
+}
+   
+  
+  
+   
+
+}
+?>
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -21,7 +64,7 @@
     </div>
     
     <section class="container my-2 bg-dark text-light w-50 p-3">
-      <form class="row g-3 r-5 form-container" action="controller.php" method="POST">
+      <form class="row g-3 r-5 form-container" action="" method="POST">
         <div class="col-md-6">
           <label for="inputFirstName" class="form-label">First name</label>
           <input type="text" class="form-control" id="inputFirstName" name="fname" required>
@@ -97,6 +140,11 @@
           <button type="submit" class="btn btn-primary">Sign in</button>
         </div>
       </form>
+      <?php if (!empty($message)): ?>
+        <div class="message <?php echo $message_type; ?>">
+            <?php echo $message; ?>
+        </div>
+    <?php endif; ?>
     </section>
   </body>
 </html>
