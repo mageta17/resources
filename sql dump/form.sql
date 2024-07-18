eeb05c6b9e36553ca2acbea1eaa01bb087966938
tamimurashid <121192802+tamimurashid@users.noreply.github.com>
2024-07-18 10:11:35 +0300

    update sql

--- sql dump/form.sql
+++ sql dump/form.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Jul 17, 2024 at 11:43 AM
+-- Generation Time: Jul 18, 2024 at 05:31 AM
 -- Server version: 5.7.34
 -- PHP Version: 7.4.21
 
@@ -40,23 +40,6 @@ CREATE TABLE `form` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `form`
---
-
-INSERT INTO `form` (`id`, `time`, `F_name`, `L_name`, `Email`, `Password`, `City`, `Gender`, `Phone`) VALUES
-(1, '2024-07-17 11:16:27', 'tamimu', 'rashid', 'rashidytamimu@gmail.com', '$2y$10$ocogsbf.S8EmJF1bRjNq3uAAJ40.0UFvoigczGkhl8M/BXox13oK6', 'Arusha', 'male', '0767938152'),
-(2, '2024-07-17 11:20:13', 'tamimu', 'rashid', 'rashidytamimu@gmail.com', '$2y$10$BqX8rq/goD4i8oOBWWYANO6vosXG2jvcFNWW7.I90En7eZ5gcaJZO', 'Arusha', 'male', '0767938152'),
-(3, '2024-07-17 11:20:59', 'tamimu', 'rashidy', 'rashidytamimu@gmail.com', '$2y$10$CWBFapfqepKImvOrrO.NPOtRedm8dGSMJ9pIaxP5kW4YYh5ftWI1O', 'arusha', 'male', '0567938152'),
-(4, '2024-07-17 11:21:52', '2323123', '1213123', 'rashidytamimu@gmail.com', '$2y$10$J8wW8RmPD9IHhcMX9TTwbugmWFe0lO/M4SOaJR1BPSmVN6a/jVK/G', 'arsuha ', 'male', '0567938152'),
-(5, '2024-07-17 11:36:08', 'tamimu', 'rashid', 'rashidytamimu@gmail.com', '$2y$10$aFjdlZflVva.4ULo6Yf2aejoIBlb/IFQfSYxm//3N/QX8v4TkZTqa', 'arusha', 'male', '0767938152'),
-(6, '2024-07-17 11:38:28', 'tamimu', 'rashid', 'rashidytamimu@gmail.com', '$2y$10$Hc.SE/3FSx2K/31NkRd1cew.AoyIPDaVP4dGbYtHdklzLBqT4aZrW', 'arusha', 'male', '0767938152'),
-(7, '2024-07-17 11:39:20', 'tamimu', 'rashid', 'rashidytamimu@gmail.com', '$2y$10$9OgnGQ.3jbu7BhRdo.0HOe.s.vhywmcIOXpIFjLWBs1aAgm6fd.SW', 'arusha', 'male', '0767938152'),
-(8, '2024-07-17 11:39:22', 'tamimu', 'rashid', 'rashidytamimu@gmail.com', '$2y$10$CsmVmoltmME5z2uHC.H.sOCRJKlHgDZsUjHcffwQawx82COuhc1OS', 'arusha', 'male', '0767938152'),
-(9, '2024-07-17 11:40:06', 'tamimu', 'rashid', 'rashidytamimu@gmail.com', '$2y$10$AjwiEqJ6Q9pC0ZAO8Ymgs.rNyyyFRUc.1oFz845f9.OyZF/cj3sGa', 'arusha', 'male', '0767938152'),
-(10, '2024-07-17 11:40:47', 'tamimu', 'rashid', 'rashidytamimu@gmail.com', '$2y$10$hFHKH8N09oaCo9N/6Tww5O5KrJqQLfQol5EfFoWf43lF1iIB2C.t.', 'Arusha', 'male', '0767938152'),
-(11, '2024-07-17 11:42:53', 'tamimu ', 'rashid', 'rashidytamimu@gmail.com', '$2y$10$sCG8W6C5k//CnH32.UPWeeO8jMJ/DBaLbv37W7Az89zZCRtaUO.ry', 'Arusha', 'male', '0767938152');
-
---
 -- Indexes for dumped tables
 --
 
@@ -74,7 +57,7 @@ ALTER TABLE `form`
 -- AUTO_INCREMENT for table `form`
 --
 ALTER TABLE `form`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
