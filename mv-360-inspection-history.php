764b870ba4fe589317005fdcc53efbd86c290999
tamimurashid <121192802+tamimurashid@users.noreply.github.com>
2024-07-23 16:44:27 +0300

    update

--- mv-360-inspection-history.php
+++ mv-360-inspection-history.php
@@ -23,85 +23,62 @@
     <div id="section" class="container-fluid mx-0 px-0">        
         <?php
             menu5();
-        ?>
+            
+            $query = "SELECT * FROM mv_check_list_360 ORDER BY id  DESC ";
 
+            $result = mysqli_query($connection, $query);
+
+            while ($row = mysqli_fetch_assoc($result)){
+                 $front_view = $row['front_view'];
+            }
+
+        ?>
         <div class="row justify-content-center mx-0">
             <div class="col-lg-6">
-                <table class="table-bordered checklist mb-3">
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>Name</th>
-                            <th>Date</th>
-                        </tr>
-                    </thead>
-
-                    <tbody>
-                        <?php
-                            // define how many results you want per page
-                            $results_per_page = 25;
-
-                            // find out the number of results stored in database
-                            $sql = "SELECT * FROM tbl_ppe_inspection where user_id='".$id."'";
-
-                            $data = mysqli_query($connection,$sql);
-                            $number_of_results = mysqli_num_rows($data);
-
-                            // determine number of total pages available
-                            $number_of_pages = ceil($number_of_results/$results_per_page);
-
-                            // determine which page number visitor is currently on
-                            if (!isset($_GET['page'])) {
-                                $page = 1;
-                            } else {
-                                $page = $_GET['page'];
-                            }
-
-                            // determine the sql LIMIT starting number for the results on the displaying page
-                            $this_page_first_result = ($page - 1) * $results_per_page;
-
-                            $sql = "SELECT * FROM tbl_ppe_inspection where user_id='".$id."' ORDER BY submission_date DESC LIMIT ". $this_page_first_result . ',' . $results_per_page;
-
-                            $data = mysqli_query($connection,$sql);
-                            $numbers = 1;
-
-                            if ($page) {
-                                $numbers = (25*$page) - 24;
-                            }
-                            if (mysqli_num_rows($data) > 0) {
-                            while($row = mysqli_fetch_array($data)) {
-                        ?> 
-                        <tr>
-                            <td>
-                                <?php
-                                    echo $numbers;
-                                    $numbers++;
-                                ?>
-                            </td>
-
-                            <td>
-                                <a href="ppe-inspection-form-view.php?id=<?php echo $row['id']; ?>"><?php echo $row['inspector_name']; ?></a>
-                            </td>
-
-                            <td>
-                                <?php echo $row['submission_date']; ?>
-                            </td>
-                        </tr>
-                        <?php
-                            }
-                            } else {
-                        ?>
-                        <tr>
-                            <td colspan="7"><center>No data to display yet!</center></td>
-                        </tr>
-                        <?php
-                            }
-                        ?>
-                    </tbody>
-                </table>
-
+                <form action="">
+                      <div class="container mt-5">
+                      <div class="row row-cols-1 row-cols-sm-2 g-3">
+                    <div class="col">
+                        <div class="card">
+                        <img src="assets/images/bs-images/img-2x1.png" class="card-img-top" alt="card-grid-image">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text"><?php echo $front_view ?></p>s
+                        </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                        <img src="assets/images/bs-images/img-2x1.png" class="card-img-top" alt="card-grid-image">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text"><?php echo $front_view ?></p>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                        <img src="assets/images/bs-images/img-2x1.png" class="card-img-top" alt="card-grid-image">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text"></p>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                        <img src="assets/images/bs-images/img-2x1.png" class="card-img-top" alt="card-grid-image">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text"></p>
+                        </div>
+                        </div>
+                    </div>
+                    </div>   
+                  </div>
+                </form>
                 <?php 
-                    include 'server/pagination.php';                           
+                    //include 'server/pagination.php';                           
                 ?>
             </div>            
         </div>
