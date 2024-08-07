5dac9a09bda9a01a99d6dae83dd931e66cb683ab
Tamim  Rashid <rashidytamimu@gmail.com>
2024-08-07 15:19:16 +0300

    update

--- mv-360-inspection-form.php
+++ mv-360-inspection-form.php
@@ -611,7 +611,17 @@
         </div>
     </div>
 
-</div>   
+</div>  
+<script>
+        // JavaScript to handle file input changes for all file inputs
+        document.querySelectorAll('input[type="file"]').forEach(function(inputElement) {
+            inputElement.addEventListener('change', function() {
+                var label = document.querySelector('label[for="' + this.id + '"]');
+                var fileName = this.files[0] ? this.files[0].name : "No file chosen";
+                label.innerHTML = '<i class="fas fa-check upload-icon"></i> File Selected';
+            });
+        });
+    </script> 
     <!-- <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script> -->
