83965bfd37e07e728b494b9b71154ebab00d1dbc
Tamim  Rashid <rashidytamimu@gmail.com>
2024-08-14 15:01:52 +0300

    update

--- server/ppe-inspection.inc.php
+++ server/ppe-inspection.inc.php
@@ -136,8 +136,8 @@ if (isset($_POST['submit'])) {
                         $totalFileSize += $file['size'];
 
                         // Check if total file size exceeds 100 MB (104857600 bytes)
-                        if ($totalFileSize > 104857600) {
-                            handleError("Total file size exceeds the limit of 100 MB.");
+                        if ($totalFileSize > 150994944) {
+                            handleError("Total file size exceeds the limit of 120 MB, each image should not exceed 5MB");
                         }
 
                         // Sanitize category name for the file name
