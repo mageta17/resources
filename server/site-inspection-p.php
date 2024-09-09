ed1124388e5374d4e37913cb4a01597a97af64df
Tamim  Rashid <rashidytamimu@gmail.com>
2024-09-09 10:20:03 +0300

    update

--- server/site-inspection-p.php
+++ server/site-inspection-p.php
@@ -232,39 +232,26 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             }
 
     } elseif($sectionId == 2){
-
-        $check_query = "SELECT id FROM site_inspection_tempo WHERE id = '$id'";
-        $result = mysqli_query($connection, $check_query);
-
-        if(mysqli_num_rows(($result)) > 0){
             $update_query = "
-        UPDATE site_inspection_tempo
-        SET 
-            response7 = '$response7', comment7 = '$comment7', action7 = '$description7',
-            response8 = '$response8', comment8 = '$comment8', action8 = '$description8',
-            response9 = '$response9', comment9 = '$comment9', action9 = '$description9'
-            WHERE id = '$id'
-             ";
-             if(mysqli_query($connection, $update_query)){
-                $insertedId = mysqli_insert_id($connection);
-                // $nextsection = $sectionId+1;
-                $_SESSION['succes2'] = "Data saved succssefully, Please procced with the next page";
-                header("Location: ../site-inspection.php?section=$sectionId&id=$insertedId");
-                exit();
-             }else{
-                $_SESSION['error2'] = "Failed to update data for section 2.";
-                header("Location: ../site-inspection.php?section=$sectionId&id=$insertedId");
-                exit();
+            UPDATE site_inspection_tempo
+            SET 
+                response7 = '$response7', comment7 = '$comment7', action7 = '$description7',
+                response8 = '$response8', comment8 = '$comment8', action8 = '$description8',
+                response9 = '$response9', comment9 = '$comment9', action9 = '$description9'
+                WHERE id = '$id'
+                ";
+                if(mysqli_query($connection, $update_query)){
+                    $insertedId = mysqli_insert_id($connection);
+                    // $nextsection = $sectionId+1;
+                    $_SESSION['succes2'] = "Data saved succssefully, Please procced with the next page";
+                    header("Location: ../site-inspection.php?section=$sectionId&id=$insertedId");
+                    exit();
+                }else{
+                    $_SESSION['error2'] = "Failed to update data for section 2.";
+                    header("Location: ../site-inspection.php?section=$sectionId&id=$insertedId");
+                    exit();
 
-             }
-        }else{
-            $_SESSION['error2'] = "Section 1 data not found, please fill in the previous section.";
-            header("Location: ../site-inspection.php?section=$sectionId&id=$insertedId");
-            exit();
-
-         }
-
-        
+                }  
 
     }else if($sectionId == 3){
 
